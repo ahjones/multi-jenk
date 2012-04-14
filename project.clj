7fede024ac7317c3ce89671633f8dc8cd0d6aeab
Andrew Jones <me@andrewhjon.es>
2012-04-14 13:59:22 +0100

    Write simple query of Jenkins

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject multi-jenk "0.1.0-SNAPSHOT"
             :description "Multi-Jenk"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.3.0-alpha10"]]
+                           [noir "1.3.0-alpha10"]
+                           [clj-http "0.3.6"]
+                           [cheshire "4.0.0"]]
             :main multi-jenk.server)
 
--- src/multi_jenk/views/welcome.clj
+++ src/multi_jenk/views/welcome.clj
@@ -1,6 +1,7 @@
 (ns multi-jenk.views.welcome
   (:require [multi-jenk.views.common :as common]
-)
+            [clj-http.client :as client]
+            [cheshire.core :as json])
   (:use [noir.core :only [defpage]]
         [noir.response :only [json]]
         [hiccup.core :only [html]]))
@@ -9,6 +10,12 @@
 
 (defpage "/servers" []
   (json jenkins-servers))
+
+(defn get-server-status [url]
+  (json/parse-string (:body (client/get url))))
+
+(defpage "/statuses" []
+  (json (get-server-status "http://33.33.33.10:8080/api/json")))
 
 (defpage "/welcome" []
          (common/layout
