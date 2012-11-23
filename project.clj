293a76c7195cf8fd90fbb0712d80e3b619ef5172
Andrew Jones <andrew.4.jones@nokia.com>
2012-11-23 11:35:05 +0000

    Update to lastest versions of deps

--- project.clj
+++ project.clj
@@ -1,21 +1,19 @@
 (defproject multi-jenk "0.1.0-SNAPSHOT"
             :description "Multi-Jenk"
-            :dependencies [[org.clojure/clojure "1.3.0"]
+            :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-alpha10"]
-                           [jayq "0.1.0-alpha2"]
-                           [clj-http "0.3.6"]
+                           [jayq "0.2.3"]
+                           [clj-http "0.5.8"]
                            [cheshire "4.0.0"]
-                           [crate "0.1.0-alpha3"]]
+                           [crate "0.2.1"]]
             :dev-dependencies [[lein-ring "0.5.4"]]
+            :source-paths ["src/clj"]
             :ring {:handler multi-jenk.server/handler}
             :cljsbuild {:builds
-                        [{:builds nil,
-                          :source-path "src"
-                          :compiler
-                          {:output-dir "resources/public/cljs"
-                           :output-to "resources/public/cljs/bootstrap.js"
-                           :optimizations :simple
-                           :pretty-print true}}]}
-            :plugins [[lein-cljsbuild "0.1.7"]]
+                        [{:source-path "src/cljs"
+                          :compiler {:output-dir "resources/public/cljs"
+                                     :output-to "resources/public/cljs/bootstrap.js"
+                                     :optimizations :simple
+                                     :pretty-print true}}]}
+            :plugins [[lein-cljsbuild "0.2.9"]]
             :main multi-jenk.server)
-
